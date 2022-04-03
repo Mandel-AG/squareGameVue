059bd8d2e1ba9111dea819292ebba9adbe170440
Mandel <a.mandel06@gmail.com>
2022-04-03 11:53:33 +0200

    structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
+    <div>
+    <span class="carre"></span>
+    <span class="carre"></span>
+  </div>
+  <div>
+    <span class="carre"></span>
+    <span class="carre"></span>
+  </div>
   </div>
 </template>
 
 <script>
 
+
 export default {
   name: 'App',
+  data(){
+    return{
+      
+    }
+  }
 }
 </script>
 
 <style>
-
+  .carre {
+    display: inline-block;
+    width:200px;
+    height:200px;
+    background-color:#ccc;
+    cursor:pointer;
+  }
 </style>
