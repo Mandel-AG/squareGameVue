0f3d13375016eb32f772f63804a064ff14987cee
Mandel <a.mandel06@gmail.com>
2022-04-03 12:55:38 +0200

    update game logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
     <button @click="newGame">New Game</button>
+    <span>Score : {}</span>
     <div>
-      <span class="carre" :class="{blue:topLeft}"></span>
-      <span class="carre" :class="{red:topRight}"></span>
+      <span class="carre" @click="selectSquare('topLeft')" :class="{blue:topLeft}"></span>
+      <span class="carre" @click="selectSquare('topRight')" :class="{red:topRight}"></span>
     </div>
     <div>
-      <span class="carre" :class="{green:bottomLeft}"></span>
-      <span class="carre" :class="{yellow:bottomRight}"></span>
+      <span class="carre" @click="selectSquare('bottomLeft')" :class="{green:bottomLeft}"></span>
+      <span class="carre" @click="selectSquare('bottomRight')" :class="{yellow:bottomRight}"></span>
     </div>
     <pre>{{ sequence }}</pre>
   </div>
@@ -25,27 +26,48 @@ export default {
       topRight: false,
       bottomLeft: false,
       bottomRight: false,
-      squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"]
+      squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"],
+      tmp:[]
     }
   },
   methods:{
     newGame() {
-      this.addNewElemToSequence();
+      this.sequence=[];
+      this.nextTurn();
     },
     addNewElemToSequence() {
-      this.sequence=[];
       this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
-      this[this.sequence[0]]= true;
-      console.log(this)
-      setTimeout(() => {
-        this.allReset()
-      }, 1000)
+      this.tmp=[...this.sequence]      
     },
     allReset(){
       this.topLeft= false,
       this.topRight=false,
       this.bottomLeft=false,
       this.bottomRight=false
+    },
+    selectSquare(element){
+      if(element === this.tmp[0]){
+        console.log('ok')
+      } else {
+        console.log('pas bon')
+      }
+    },
+    playSequence(element){
+      this[element]= true;
+      setTimeout(() => {
+          this.allReset()
+          this.tmp.shift()
+          if(this.tmp[0]){
+            this.playSequence(this.tmp[0])
+          }else{
+            this.tmp=[...this.sequence]
+          }
+      }, 450)
+    },
+    nextTurn(){
+      this.addNewElemToSequence()
+      this.allReset()
+      this.playSequence(this.tmp[0])
     }
   }
 }
