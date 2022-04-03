1f56ce650ac368980a27dbc5137c8656fd39c9e2
Mandel <a.mandel06@gmail.com>
2022-04-03 12:23:36 +0200

    update sequence array

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <button @click="newGame">New Game</button>
     <div>
-    <span class="carre" :class="{blue:topLeft}"></span>
-    <span class="carre" :class="{red:topRight}"></span>
-  </div>
-  <div>
-    <span class="carre" :class="{green:bottomLeft}"></span>
-    <span class="carre" :class="{yellow:bottomRight}"></span>
-  </div>
+      <span class="carre" :class="{blue:topLeft}"></span>
+      <span class="carre" :class="{red:topRight}"></span>
+    </div>
+    <div>
+      <span class="carre" :class="{green:bottomLeft}"></span>
+      <span class="carre" :class="{yellow:bottomRight}"></span>
+    </div>
+    <pre>{{ sequence }}</pre>
   </div>
 </template>
 
@@ -24,6 +26,26 @@ export default {
       bottomLeft: false,
       bottomRight: false,
       squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"]
+    }
+  },
+  methods:{
+    newGame() {
+      this.addNewElemToSequence();
+    },
+    addNewElemToSequence() {
+      this.sequence=[];
+      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+      this[this.sequence[0]]= true;
+      console.log(this)
+      setTimeout(() => {
+        this.allReset()
+      }, 1000)
+    },
+    allReset(){
+      this.topLeft= false,
+      this.topRight=false,
+      this.bottomLeft=false,
+      this.bottomRight=false
     }
   }
 }
