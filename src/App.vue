80cd72ef4874a55f2373acfbe97a5b0888990d6c
badel <a.mandel06@gmail.com>
2023-11-24 18:42:18 +0100

    remove sequences

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,125 @@
 <template>
   <div id="app">
     <button @click="newGame">New Game</button>
-    <span>Score : {{score}}</span>
+    <span>Score : {{ score }}</span>
     <div>
-      <span class="carre" @click="selectSquare('topLeft')" :class="{blue:topLeft}"></span>
-      <span class="carre" @click="selectSquare('topRight')" :class="{red:topRight}"></span>
+      <span
+        class="carre"
+        @click="selectSquare('topLeft')"
+        :class="{ blue: topLeft }"
+      ></span>
+      <span
+        class="carre"
+        @click="selectSquare('topRight')"
+        :class="{ red: topRight }"
+      ></span>
     </div>
     <div>
-      <span class="carre" @click="selectSquare('bottomLeft')" :class="{green:bottomLeft}"></span>
-      <span class="carre" @click="selectSquare('bottomRight')" :class="{yellow:bottomRight}"></span>
+      <span
+        class="carre"
+        @click="selectSquare('bottomLeft')"
+        :class="{ green: bottomLeft }"
+      ></span>
+      <span
+        class="carre"
+        @click="selectSquare('bottomRight')"
+        :class="{ yellow: bottomRight }"
+      ></span>
     </div>
-    <pre>{{ sequence }}</pre>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  data(){
-    return{
+  name: "App",
+  data() {
+    return {
       sequence: [],
       topLeft: false,
       topRight: false,
       bottomLeft: false,
       bottomRight: false,
       squareMapping: ["topLeft", "topRight", "bottomLeft", "bottomRight"],
-      tmp:[]
-    }
+      tmp: [],
+    };
   },
-  computed:{
-    score(){
-      const value = this.sequence.length -1
-      return (value < 0) ? 0  : value ; 
-    }
+  computed: {
+    score() {
+      const value = this.sequence.length - 1;
+      return value < 0 ? 0 : value;
+    },
   },
-  methods:{
+  methods: {
     newGame() {
-      this.sequence=[];
+      this.sequence = [];
       this.nextTurn();
     },
     addNewElemToSequence() {
-      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
-      this.tmp=[...this.sequence]      
+      this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+      this.tmp = [...this.sequence];
     },
-    allReset(){
-      this.topLeft= false,
-      this.topRight=false,
-      this.bottomLeft=false,
-      this.bottomRight=false
+    allReset() {
+      (this.topLeft = false),
+        (this.topRight = false),
+        (this.bottomLeft = false),
+        (this.bottomRight = false);
     },
-    selectSquare(carre){
-      if(carre === this.tmp[0]){
-        this[carre]=true;
-        setTimeout(()=>{
-          this.allReset()
-          this.tmp.shift()
-          if(!this.tmp[0]){
-            this.nextTurn()
+    selectSquare(carre) {
+      if (carre === this.tmp[0]) {
+        this[carre] = true;
+        setTimeout(() => {
+          this.allReset();
+          this.tmp.shift();
+          if (!this.tmp[0]) {
+            this.nextTurn();
           }
-        },400)
+        }, 400);
       } else {
-        alert('perdu')
+        alert("perdu");
       }
     },
-    playSequence(element){
-      this[element]= true;
+    playSequence(element) {
+      this[element] = true;
       setTimeout(() => {
-          this.allReset()
-          this.tmp.shift()
-          if(this.tmp[0]){
-            setTimeout(()=>{
-              this.playSequence(this.tmp[0])
-            },400)
-          }else{
-            this.tmp=[...this.sequence]
-          }
-      }, 450)
+        this.allReset();
+        this.tmp.shift();
+        if (this.tmp[0]) {
+          setTimeout(() => {
+            this.playSequence(this.tmp[0]);
+          }, 400);
+        } else {
+          this.tmp = [...this.sequence];
+        }
+      }, 450);
     },
-    nextTurn(){
-      this.addNewElemToSequence()
-      this.allReset()
-      this.playSequence(this.tmp[0])
-    }
-  }
-}
+    nextTurn() {
+      this.addNewElemToSequence();
+      this.allReset();
+      this.playSequence(this.tmp[0]);
+    },
+  },
+};
 </script>
 
 <style>
-  .carre {
-    display: inline-block;
-    width:200px;
-    height:200px;
-    background-color:#ccc;
-    cursor:pointer;
-    margin: 1em;
-  }
-  .blue {
-  background-color:blue;
+.carre {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  background-color: #ccc;
+  cursor: pointer;
+  margin: 1em;
+}
+.blue {
+  background-color: blue;
 }
 .red {
-  background-color:red;
+  background-color: red;
 }
 .green {
-  background-color:green;
+  background-color: green;
 }
 .yellow {
-  background-color:yellow;
+  background-color: yellow;
 }
 </style>
